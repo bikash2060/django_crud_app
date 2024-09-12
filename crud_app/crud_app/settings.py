59c08a248d96650d6dba150fde6346efa9bcef3d
bikash2060 <bkashbhattarai@gmail.com>
2024-09-12 18:00:38 +0545

    New app created

--- crud_app/crud_app/settings.py
+++ crud_app/crud_app/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = "crud_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'Bishal@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
