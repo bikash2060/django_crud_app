2adc10bb40e7fa9686a347984e06ee109b9f70bd
bikash2060 <bkashbhattarai@gmail.com>
2024-09-12 18:43:39 +0545

    Templates routing completed

--- crud_app/mainapp/urls.py
+++ crud_app/mainapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.showProductListView, name="product-list"),
+    path("product-list/", views.showProductListView, name="product-list"),
     path("add-product/", views.showAddProductView, name="add-product"),
-    path("edit-product/", views.showEditProductView, name="edit-list"),
+    path("edit-product/", views.showEditProductView, name="edit-product"),
 ]
