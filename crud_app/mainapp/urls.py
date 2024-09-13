471d1d42f785191639374f511b7e911be88e6e88
bikash2060 <bkashbhattarai@gmail.com>
2024-09-13 15:36:53 +0545

    URLS modified

--- crud_app/mainapp/urls.py
+++ crud_app/mainapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("products/", views.showProductListView, name="product-list"),
     path("add-product/", views.showAddProductView, name="add-product"),
-    path("edit-product/", views.showEditProductView, name="edit-product"),
+    path("edit-product/<int:id>", views.showEditProductView, name="edit-product"),
 ]
