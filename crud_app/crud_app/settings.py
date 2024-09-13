3314610ecfae4322ac4e317c4359dbaf8529c8dd
bikash2060 <bkashbhattarai@gmail.com>
2024-09-13 21:53:08 +0545

    CRUD Operation Completed

--- crud_app/crud_app/settings.py
+++ crud_app/crud_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-98#)bzbc7)vutj1gn^r)h_!(ie_wl+a#)9hj!8zp36ib*z9#6r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
