175ff32a9dc2da3c15db303a76d3847fc3f1c7b8
bikash2060 <bkashbhattarai@gmail.com>
2024-09-16 19:32:53 +0545

    Models modified

--- crud_app/mainapp/models.py
+++ crud_app/mainapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Product(models.Model):
-    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     ProductID = models.AutoField(primary_key=True)
     ProductName = models.CharField(max_length=200)
     ProductPrice = models.FloatField()  
