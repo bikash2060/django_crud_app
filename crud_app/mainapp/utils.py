a42eb9ab78e5cd7e28431177776b73e390216ccd
bikash2060 <bkashbhattarai@gmail.com>
2024-09-16 19:33:51 +0545

    New form parameters added

--- crud_app/mainapp/utils.py
+++ crud_app/mainapp/utils.py
@@ -1,6 +1,13 @@
 # Parameters names of add product form
-productName = "productName"
-productCategory = "category"
-productQuantity = "quantity"
-productPrice = "price"
-productImage = "image"
+PRODUCT_NAME = "productName"
+PRODUCT_CATEGORY = "category"
+PRODUCT_QUANTITY = "quantity"
+PRODUCT_PRICE = "price"
+PRODUCT_IMAGE = "image"
+
+# Parameters names for signup page
+FIRST_NAME = "firstName"
+LAST_NAME = "lastName"
+EMAIL_ADDRESS = "emailAddress"
+USERNAME = "username"
+PASSWORD = "password"
