c90d0ecd426cc347339a3ff057385dba8dc1d2d9
bikash2060 <bkashbhattarai@gmail.com>
2024-09-18 14:58:13 +0545

    New parameters added

--- crud_app/mainapp/utils.py
+++ crud_app/mainapp/utils.py
@@ -5,10 +5,14 @@ PRODUCT_QUANTITY = "quantity"
 PRODUCT_PRICE = "price"
 PRODUCT_IMAGE = "image"
 
-# Parameters names for signup page
+# Parameters names for signup and login page
 FIRST_NAME = "firstName"
 LAST_NAME = "lastName"
 EMAIL_ADDRESS = "emailAddress"
 USERNAME = "username"
 PASSWORD = "password"
 RETYPED_PASSWORD = "retypePassword"
+
+# Other parameters
+SEARCH = "search"
+SORT = "sort"
